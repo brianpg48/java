97dbb7478ff959ac384cee97615921017bb13480
brianpg48 <brian.r@aluno.senai.br>
2025-03-26 16:51:01 -0300

    Add files via upload

---
+++ Teste/script.js
@@ -0,0 +1,23 @@
+var nota1 = 8;
+var nota2 = 8;
+var nota3 = 8;
+
+var media = (nota1+nota2+nota3) / 3; 
+console.log(media);
+
+if (media<7){
+    console.log("Reprovado");
+}else{
+    console.log("Aprovado");
+};
+/**Estrutura de repetição */
+for(var i = 0; i < 5; i++){
+ console.log(i);
+ console.log("Brian");
+}
+var repeticao = 10;
+var index = 1;
+while(index <=10){
+    console.log("repeticao", index);
+    index++;
+}
